253770b5c928bb75710b17a60e94a147517c23de
James Tsukamoto <jaytsuk@gmail.com>
2019-01-08 11:43:49 -0800

    production build

--- server/index.js
+++ server/index.js
@@ -4,21 +4,20 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(morgan('dev'));
 
 // Reviews - James
-app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
-app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
+app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
+app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
 
 // Image Gallery - Josh
-app.use('/rooms/:id/photos', proxy({target: 'http://localhost:1337'}));
+app.use('/rooms/:id/photos', proxy({target: 'http://54.175.98.175'}));
 
 // Booking - Kevin
-app.use('/api/rooms/:id', proxy({target: 'http://localhost:8080'}));
+app.use('/api/rooms/:id', proxy({target: 'http://54.67.99.254'}));
 
 // Recommendations - Eric
-app.use('/house', proxy({target: 'http://localhost:3123'}));
+app.use('/house', proxy({target: 'http://18.223.185.89'}));
 
 app.use('/rooms/:roomid', express.static("./public"));
 
